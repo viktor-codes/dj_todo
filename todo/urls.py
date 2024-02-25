826dd40b3f959ae9d1b1a9caffbe9824d0cab2fd
Viktor Rura <ruravictor@gmail.com>
2024-02-25 14:03:10 +0000

    Added info to README.md file, tests and updated style.css and index.html

--- todo/urls.py
+++ todo/urls.py
@@ -20,8 +20,12 @@ urlpatterns = [
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path(
+        "tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"
+    ),
 
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
